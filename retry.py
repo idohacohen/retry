e1d333401a27d7aa11634639c9ea46a4de357d35
idohacohen <idohacohen1@gmail.com>
2025-11-06 01:22:59 +0200

    done

---
+++ retry.py
@@ -0,0 +1,28 @@
+import random
+from typing import Callable
+from functools import wraps
+
+
+def retry(func: Callable) -> Callable:
+    """decorator that retries a function until it returns something other than None"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        success = False
+        while not success:
+            result = func(*args, **kwargs)
+            if result is not None:    
+                success = True
+        return result
+    return wrapper
+
+
+@retry
+def return_three() -> int|None:
+    """returns the number 3 randomly, otherwise None"""
+    number = random.randint(1, 5)
+    if number == 3:
+        return number
+    
+
+if __name__ == "__main__":
+    print(return_three())
